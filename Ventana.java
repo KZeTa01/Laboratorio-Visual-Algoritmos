6848e23f8abd625234b8b2357b1f615fa606f78e
Yair <sosaruizyairalexander@gmail.com>
2026-07-10 17:25:15 -0500

    Ordenamiento de codigo 1

--- Ventana.java
+++ Ventana.java
@@ -9,12 +9,20 @@ public class Ventana extends JFrame implements ActionListener{
     JTextArea Descripcion;
     
     public Ventana(){
+        confiurarVentana();
+        inicializarComponentes();
+        mostrarDescripcion();
+    }
+
+    private void confiurarVentana(){
         setSize(900,700);
         setLayout(new BorderLayout());
         setLocationRelativeTo(null);
         setTitle("Laboratorio Visual de Algoritmos");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        
+    }
+
+    public void inicializarComponentes(){
         //Crear Fuentes
         Font fTitulo = new Font("Times New Roman", Font.BOLD,30);
         Font fSubtemas = new Font("Times New Roman", Font.BOLD, 20);
@@ -71,8 +79,6 @@ public class Ventana extends JFrame implements ActionListener{
         add(titulo, BorderLayout.NORTH);
         add(pDescripcion, BorderLayout.EAST);
         add(pAlgoritmos, BorderLayout.CENTER);
-
-        mostrarDescripcion();
     }
     
     @Override
